362b81c578debb9892393e20f6cf2b89209a4aca
Naumenko Bohdan <34416971+DedalJun@users.noreply.github.com>
2018-03-05 13:25:37 +0200

    Add files via upload

---
+++ Simplex_Method_V2.0/Simplex_Method_V2.0/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Simplex_Method_V2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(149, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(473, 138);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(149, 23);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(502, 23);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(197, 270);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(372, 100);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(652, 106);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Max";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(652, 139);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Min";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 422);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Simplex_Method_V2.0/Simplex_Method_V2.0/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Simplex_Method_V2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n, m;
+
+        public void UpdateDataGrid()
+        {
+            dataGridView1.Columns.Clear();
+
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                dataGridView1.Columns.Add("", "X" + (i + 1).ToString());
+                if (i == Convert.ToInt16(numericUpDown1.Value - 1))
+                {
+                    dataGridView1.Columns.Add("", "<=>");
+                    dataGridView1.Columns.Add("", "B");
+                }
+            }
+
+            for (int j = 0; j < numericUpDown2.Value; j++)
+            {
+                dataGridView1.Rows.Add();
+            }
+
+            m = Convert.ToInt16(numericUpDown1.Value);
+            n = Convert.ToInt16(numericUpDown2.Value);
+
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateDataGrid();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateDataGrid();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[,] data = new double[n + 2, m * m + 2];
+            double[,] newdata = new double[n + 2, m * m + 2];
+            int[] sol = new int[n];
+
+            double freelement, tmp;
+            int mainrow = 0, maincol = 1, i, j = 0, g, countcols, M, end = 0;
+            bool fl = false, w = false, v = false;
+            countcols = n + m;
+            M = m + n + 2;
+            ////////////////////////////Построение массива с входными данными//////////////////////////////
+            for (i = 0; i <= n; i++)
+            {
+                data[i, 0] = Convert.ToDouble(dataGridView1[m + 1, i].Value);
+                for (j = 0; j < m; j++)
+                {
+                    data[i, j + 1] = Convert.ToDouble(dataGridView1[j, i].Value);
+                }
+            }
+
+            for (i = 0; i <= n; i++)
+                for (j = m + 1; j <= m + n; j++)
+                {
+                    if ((j - (m + 1)) == i)
+                    {
+                        char s = Convert.ToChar(dataGridView1[m, i].Value);
+                        if (s == '<') data[i, j] = 1;
+                        else if (s == '0') data[i, j] = 0;
+                        else
+                        {
+
+                            if (s != '=')
+                            {
+                                countcols++;
+                                data[i, j] = -1;
+                            }
+                            data[i, countcols] = 1;
+                            w = true;
+                            for (g = 0; g < m + n; g++)
+                            {
+                                data[n + 1, g] -= data[i, g];
+                            }
+                        }
+                    }
+                    else data[i, j] = 0;
+                }
+
+
+            ////////////CHECK//////////////
+            for (i = 0; i <= n + 1; i++)
+            {
+                for (j = 0; j <= countcols; j++)
+                {
+                    richTextBox1.Text = richTextBox1.Text + Convert.ToString(data[i, j]) + "  ";
+                }
+                richTextBox1.Text = richTextBox1.Text + "\n";
+            }
+            richTextBox1.Text = richTextBox1.Text + "\n";
+            //////////////////////////////
+
+            //////////////////////////////////////////////////////////////////////////////////////////////
+
+            ////////////////////////////////////////////-------START SIMPLEX-------///////////////////////////////////////////////////
+
+            
+            //----------///////////////////Начало работы вычисления исскуственной целевой ф-ции///////////////////////--------------//
+
+            while (w)
+            {
+                w = false;
+                ///////////////////////Определение главного столбца///////////////////////////
+                tmp = data[n + 1, 1];
+
+                for (j = 1; j <= m + n; j++)
+                {
+                    if (data[n + 1, j] < tmp)
+                    {
+                        tmp = data[n + 1, j];
+                        maincol = j;
+                    }
+                }
+                /////////////////////////////////////////////////////////////////////////////
+
+                tmp = 11011;
+                for (i = 0; i < n; i++)
+                {
+                    if (data[i, maincol] > 0)
+                    {
+                        data[i, countcols + 1] = data[i, 0] / data[i, maincol];
+                        if (tmp == 11011)
+                        {
+                            tmp = data[i, countcols + 1];
+                            mainrow = i;
+                        }
+                    }     //////////min = B[i]/A[i];
+                    else
+                    {
+                        data[i, countcols + 1] = -1;
+                        end++;
+                    }
+                    //if (data[i, countcols + 1] >= 0)
+                    //{
+                    //    tmp = data[i, countcols + 1];
+                    //    mainrow = i;
+                    //}
+                }
+
+                if (end == n)
+                {
+                    MessageBox.Show("Solving not found");
+                    break;
+                }
+
+
+                ///////////////////////Определение главной строки////////////////////////////
+
+                for (i = 0; i < n; i++)
+                {
+                    if ((data[i, countcols + 1] < tmp) && (data[i, countcols + 1] >= 0))
+                    {
+                        tmp = data[i, countcols + 1];
+                        mainrow = i;
+                    }
+                }
+
+                freelement = data[mainrow, maincol];
+                MessageBox.Show(Convert.ToString(freelement));
+                sol[mainrow] = maincol;
+                /////////////////////////////////////////////////////////////////////////////
+
+                //////////////Пересчёт нового плана методом прямоугольника///////////////////
+                for (i = 0; i <= n + 1; i++)
+                {
+                    if (i == mainrow)
+                        for (g = 0; g <= countcols; g++)
+                        {
+                            newdata[i, g] = data[i, g] / freelement;
+                        }
+                    else
+                        for (j = 0; j <= countcols; j++)
+                        {
+                            if (j == maincol) newdata[i, j] = 0;
+                            else
+                            {
+                                newdata[i, j] = Math.Round(data[i, j] - (data[mainrow, j] * data[i, maincol]) / freelement, 2, MidpointRounding.ToEven);
+                            }
+
+                            if (j == maincol && i == mainrow) newdata[i, j] = 1;
+                        }
+                }
+                /////////////////////////////////////////////////////////////////////////////
+
+                /////////////////////////////////PRINT//////////////////////////////////////
+                for (i = 0; i <= n + 1; i++)
+                {
+                    for (j = 0; j <= countcols; j++)
+                    {
+                        richTextBox1.Text = richTextBox1.Text + Convert.ToString(newdata[i, j]) + "  ";
+                    }
+                    richTextBox1.Text = richTextBox1.Text + "\n";
+                }
+                richTextBox1.Text = richTextBox1.Text + "\n";
+                ////////////////////////////////////////////////////////////////////////////
+
+                //////////////////Копирование новых даных поверх старых/////////////////////
+                for (i = 0; i <= n + 1; i++)
+                {
+                    for (j = 0; j < countcols; j++)
+                    {
+                        data[i, j] = newdata[i, j];
+                    }
+                }
+                ////////////////////////////////////////////////////////////////////////////
+
+                ///////////////////////////Проверка на зевершение///////////////////////////
+                for (j = 0; j < m + n; j++)
+                {
+                    if (newdata[n + 1, j] != 0) w = true;
+                }
+                ////////////////////////////////////////////////////////////////////////////
+            }
+            //---------//////////////////////////////////////////////////////////////////////////////////////////----------//
+
+            ////--------////////////////////////////Начало работы симплекс метода////////////////////////////////---------////
+            ////
+            countcols = m + n;
+            ///////////////////////////Проверка на зaвершение///////////////////////////
+            for (j = 1; j < m + n; j++)
+            {
+                if (newdata[n, j] >= 0) fl = true;
+            }
+            ////////////////////////////////////////////////////////////////////////////
+
+
+
+            while (fl)
+            {
+                fl = false;
+                ///////////////////////Определение главного столбца///////////////////////////
+                tmp = data[n, 1];
+                maincol = 1;
+
+                if (radioButton1.Checked)
+                {
+                    for (j = 1; j <= m + n; j++)
+                    {
+                        if (data[n, j] > tmp)
+                        {
+                            tmp = data[n, j];
+                            maincol = j;
+                        }
+                    }
+                }
+                else
+                {
+                    for (j = 1; j <= m + n; j++)
+                    {
+                        if (data[n, j] < tmp)
+                        {
+                            tmp = data[n, j];
+                            maincol = j;
+                        }
+                    }
+                }
+                /////////////////////////////////////////////////////////////////////////////
+
+                end = 0;
+                tmp = 11011;
+                for (i = 0; i < n; i++)
+                {
+                    if (data[i, maincol] > 0)
+                    {
+                        data[i, countcols + 1] = data[i, 0] / data[i, maincol];
+                        if (tmp == 11011)
+                        {
+                            tmp = data[i, countcols + 1];
+                            mainrow = i;
+                        }
+                    }     //////////min = B[i]/A[i];
+                    else
+                    {
+                        data[i, countcols + 1] = -1;
+                        end++;
+                    }
+                    //if (data[i, countcols + 1] >= 0)
+                    //{
+                    //    tmp = data[i, countcols + 1];
+                    //    mainrow = i;
+                    //}
+                }
+
+                if (end == n)
+                {
+                    MessageBox.Show("Solving not found");
+                    break;
+                }
+
+                ///////////////////////Определение главной строки////////////////////////////
+
+
+                for (i = 0; i < n; i++)
+                {
+                    if ((data[i, countcols + 1] <= tmp) && (data[i, countcols + 1] >= 0))
+                    {
+                        tmp = data[i, countcols + 1];
+                        mainrow = i;
+                    }
+                }
+
+                freelement = data[mainrow, maincol];
+                sol[mainrow] = maincol;
+                /////////////////////////////////////////////////////////////////////////////
+
+                //////////////Пересчёт нового плана методом прямоугольника///////////////////
+                for (i = 0; i <= n; i++)
+                {
+                    if (i == mainrow)
+                        for (g = 0; g <= countcols; g++)
+                        {
+                            newdata[i, g] = data[i, g] / freelement;
+                        }
+                    else
+                        for (j = 0; j <= countcols; j++)
+                        {
+                            if (j == maincol) newdata[i, j] = 0;
+                            else
+                            {
+                                newdata[i, j] = Math.Round(data[i, j] - (data[mainrow, j] * data[i, maincol]) / freelement , 2, MidpointRounding.ToEven);
+                            }
+
+                            if (j == maincol && i == mainrow) newdata[i, j] = 1;
+                        }
+                }
+                /////////////////////////////////////////////////////////////////////////////
+
+                /////////////////////////////////PRINT//////////////////////////////////////
+                for (i = 0; i <= n; i++)
+                {
+                    for (j = 0; j < countcols; j++)
+                    {
+                        richTextBox1.Text = richTextBox1.Text + Convert.ToString(Math.Round(newdata[i, j],2)) + "  ";
+                    }
+                    richTextBox1.Text = richTextBox1.Text + "\n";
+                }
+                richTextBox1.Text = richTextBox1.Text + "\n";
+                ////////////////////////////////////////////////////////////////////////////
+
+                //////////////////Копирование новых даных поверх старых/////////////////////
+                for (i = 0; i <= n; i++)
+                {
+                    for (j = 0; j < countcols; j++)
+                    {
+                        data[i, j] = newdata[i, j];
+                    }
+                }
+                ////////////////////////////////////////////////////////////////////////////
+
+                ///////////////////////////Проверка на зевершение///////////////////////////
+                if (radioButton1.Checked)
+                {
+                    for (j = 1; j < m + n; j++)
+                    {
+                        if (newdata[n, j] > 0) fl = true;
+                    }
+                }
+                else
+                {
+                    for (j = 1; j < m + n; j++)
+                    {
+                        if (newdata[n, j] < 0) fl = true;
+                    }
+                }
+                ////////////////////////////////////////////////////////////////////////////
+                
+            }
+
+            MessageBox.Show("Result - " + Convert.ToString(newdata[n, 0] * (-1)));
+            richTextBox1.Text = richTextBox1.Text + "\nResult - " + Convert.ToString(newdata[n, 0] * (-1));
+
+            for (i = 0; i < n; i++)
+            {
+                if (sol[i] <= n && sol[i] != 0) richTextBox1.Text = richTextBox1.Text + "\nX" + Convert.ToString(sol[i]) + " = " + Convert.ToString(newdata[i, 0]);
+                else v = true;
+            }
+
+            if (v) richTextBox1.Text = richTextBox1.Text + " Others variable = 0";
+        }
+
+    }
+}
